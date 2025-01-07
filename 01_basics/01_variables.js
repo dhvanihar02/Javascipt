1d0485205e08541bf7240c5166f59674baeb8639
dhvanihar02 <dhvaniharjai02@gmail.com>
2025-01-07 18:44:36 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 1223 //constant -> cannot be changed 
+let accountEmail = "dhvani@gmail.com" //can be changed
+let accountState; //value is undefined
+var accountpw = "1234" //not preferred
+accountCity = "Mumbai" //not preferred
+
+console.table([accountId,accountEmail,accountState,accountpw,accountCity])
