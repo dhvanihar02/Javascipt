bb14cd5a41fd4c2b83ce23cca5d79db0715dd3b1
dhvanihar02 <dhvaniharjai02@gmail.com>
2025-01-07 18:59:34 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"user strict";
+let age = 14
+let isHappy = true
+let name = "Dhvani"
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof isHappy)
