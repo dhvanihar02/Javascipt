30b5cd5cec74c573e09bdc505869d4e55272a894
dhvanihar02 <dhvaniharjai02@gmail.com>
2025-01-17 14:49:43 +0000

    COMPARISONS

---
+++ 01_basics/04_Comparisons.js
@@ -0,0 +1,11 @@
+console.log(2 > 1)
+console.log("2" > 1) //consider 2 number for comparison
+console.log(null>0) // consider null as 0 
+console.log(null==0) // this operator works different and does not consider null as zero
+console.log(null>=0) // consider null as 0
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// AVOID COMPARISONS OF NULL AND UNDEFINED 
